83d84729784dceb6323c55ab3bfb2659cd4b27a9
Richard Hanks <richardhanksjr@gmail.com>
2017-11-13 13:43:27 -0500

    first

---
+++ learning_templates/basic_app/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+def index(request):
+    context = {'text': 'hello world',
+               'number': 100}
+    return render(request, 'basic_app/index.html', context=context)
+
+
+def other(request):
+    return render(request, 'basic_app/other.html')
+
+def relative(request):
+    return render(request, 'basic_app/relative_url_templates.html')
